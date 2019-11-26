581b26c238711bacc0e54b51f98de0d7a8be0ea9
ys2n <ys2n@virginia.edu>
2019-11-26 11:24:44 -0500

    Not wholly working

--- express/server.js
+++ express/server.js
@@ -10,7 +10,7 @@ const REDIS_PORT = 6379;
 const REDIS_TTL = 86400;
 const REDIS_PASS = "W67lPiuZtb6V";
 const redis = require('redis');
-const redisClient = redis.createClient({ url: process.env.REDIS_URL, password: REDIS_PASS });
+const redisClient = redis.createClient({url: process.env.REDIS_URL, password: REDIS_PASS});
 const redisStore = require('connect-redis')(session);
 
 // Constants
@@ -37,7 +37,7 @@ app.use(
         store: new redisStore({
             host: process.env.REDIS_URL,
             port: REDIS_PORT,
-	    password: REDIS_PASS,
+            password: REDIS_PASS,
             client: redisClient,
             ttl: REDIS_TTL
         })
@@ -48,46 +48,99 @@ app.get('/', function (req, res) {
 });
 
 // OAuth Redirect Route
-app.get('/oauth2/redirect', (req, res, next) => {
+app.get('/oauth2/redirect', async (req, res, next) => {
     const requestToken = req.query.code
-    axios({
-        method: 'post',
-        // url: `https://mandala-dev.shanti.virginia.edu/oauth2/token`,
-        // url: "https://mandala.dd:8443/oauth2/token",
-        url: process.env.MANDALA_URL + "/oauth2/token",
-        data: {
-            "grant_type": "client_credentials",
-            "client_id": OAuth_clientID,
-            "client_secret": OAuth_clientSecret,
-	        "scope": "openid profile email basic",
-            "code": requestToken
-        },
-        headers: {
-            accept: 'application/json'
-        }
-    }).then((response) => {
-        // console.log("We got a response!");
-        // console.log(response.data);
-        const token_json = response.data;
+    const CSRF_TOKEN_URL = process.env.MANDALA_URL + "/services/session/token";
+    const OAUTH2_TOKEN_URL = process.env.MANDALA_URL + "/oauth2/token";
 
-    	console.log( "Probably should do some validation / verification here!");
-	    console.dir(token_json);
+    let client = axios.create(
+        {withCredentials: true}
+    );
+
+    let csrf = "";
+    try {
+        let resp = await client.get(CSRF_TOKEN_URL);
+        csrf = resp.data;
+        console.log("got CSRF = " + csrf);
+        req.session["csrf_token"] = csrf;
+    } catch (err) {
+        const errorMsg = "Couldn't get CSRF token from " + CSRF_TOKEN_URL + " Error: " + err;
+        console.error(errorMsg, err);
+        next(err);
+        return;
+    }
+
+    try {
+        console.log("BEFORE THE POST");
+        let response = await client.post(OAUTH2_TOKEN_URL,
+            {
+                "grant_type": "client_credentials",
+                "client_id": OAuth_clientID,
+                "client_secret": OAuth_clientSecret,
+                "scope": "openid profile email basic",
+                "code": requestToken
+            },
+            {
+                headers: {
+                    "accept": 'application/json',
+                    "x-csrf-token": csrf
+                }
+            }
+        )
+        console.log("AFTER THE POST");
+        let token_json = response.data;
+        console.log("GOT token = " + token_json);
+        console.dir(token_json);
         req.session["access_token"] = token_json;
         res.redirect(`/process`);
-    }).catch(error => {
-        console.log(error);
-        next(error);
-    });
+
+    } catch (err) {
+        const errorMsg = "Couldn't get OAuth2 token from " + OAUTH2_TOKEN_URL + " Error: " + err;
+        console.error(errorMsg, err);
+        res.send("ERROR: " + errorMsg);
+        next(err);
+        return;
+    }
 })
 
 // Login path.  Initiates the OAuth ping pong match...
 app.get("/login", (req, res, next) => {
-	res.redirect(process.env.MANDALA_URL + "/oauth2/authorize?client_id=test");
+    res.redirect(process.env.MANDALA_URL + "/oauth2/authorize?client_id=test");
 });
 
 // Should be authorized now
 app.get("/process", (req, res, next) => {
     // We should have authorization token now
+
+    let access = req.session["access_token"].access_token;
+    let xcsrf = req.session["csrf_token"];
+
+    axios({
+        method: 'get',
+        url: process.env.MANDALA_URL + "/oauth2/UserInfo",
+        headers: {
+            accept: 'application/json',
+            Authorization: "Bearer " + access,
+            "X-CSRF-Token": xcsrf
+        }
+    }).then((response) => {
+        // console.log("We got a response!");
+        // console.log(response.data);
+        const data = response.data;
+        res.send("We got a response!<p>\n" +
+            "<pre>" + JSON.stringify(data, undefined, 2) + "</pre>\n" +
+            "<h2>TOKENS</h2>" +
+            "<ul>" +
+            "<li>access_token: " + JSON.stringify(req.session["access_token"]) + "</li>" +
+            "<li>csrf_token: " + req.session["csrf_token"] + "</li>" +
+            "</ul>");
+    }).catch(error => {
+        console.log(error);
+        next(error);
+    });
+
+    console.log("PROCESSED: session:");
+    console.dir(req.session);
 });
 
 // Proxy
@@ -113,5 +166,6 @@ app.use('/solr', proxy('https://ss251856-us-east-1-aws.measuredsearch.com', {  /
 }));
 
 
-
 app.listen(PORT);
+console.log("express is listening on port " + PORT);
+
