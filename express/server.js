9acaf04ce2730aa2a3a400c0f30fd17ffe58b7cb
ys2n <ys2n@virginia.edu>
2019-11-26 04:04:37 +0000

    intermediate check-in

--- express/server.js
+++ express/server.js
@@ -22,6 +22,8 @@ const app = express();
 // OAuth configs
 const OAuth_clientID = "test";
 const OAuth_clientSecret = "12345";
+// const OAuth_clientID = "z45bh4bk9wggJjRhdzJ2K2vKEJXKZwxe";
+// const OAuth_clientSecret = "D2UZz6nbGi6ULtyBZyyeAxbKpvcpZY5u";
 
 // Plainjane static pages
 app.use(express.static('public'))
@@ -30,10 +32,10 @@ app.use(express.static('public'))
 app.use(
     session({
         secret: 'redissessionsecretshush',
-        name: 'solrProxyApp',
+        name: 'solrProxySession',
         resave: false,
         saveUninitialized: true,
-        cookie: {secure: false},
+        cookie: {secure: true},
         store: new redisStore({
             host: process.env.REDIS_URL,
             port: REDIS_PORT,
@@ -49,50 +51,74 @@ app.get('/', function (req, res) {
 
 // OAuth Redirect Route
 app.get('/oauth2/redirect', (req, res, next) => {
-    const requestToken = req.query.code
-    axios({
-        method: 'post',
-        url: `https://mandala-dev.shanti.virginia.edu/oauth2/token`,
-        data: {
+    const requestToken = req.query.code;
+    const client = axios.create({
+	withCredentials: true;
+    });
+
+    client.get('https://mandala-dev.shanti.virginia.edu/session/token').then(csrf) => {
+    client.post('https://mandala-dev.shanti.virginia.edu/oauth2/token',
+        {
             "grant_type": "client_credentials",
             "client_id": OAuth_clientID,
             "client_secret": OAuth_clientSecret,
+	    "scope": "openid profile email basic",
             "code": requestToken
         },
+        {    
+            headers: {
+                 accept: 'application/json'
+            }
+         }
+    }).then((response) => {
+        console.log("We got a response!");
+        console.log(response);
+        const token_json = response.data;
+
+	console.log( "Probably should do some validation / verification here!");
+        req.session["access_token"] = token_json;
+	console.dir(req.session);
+
+    const access = req.session["access_token"].access_token; 
+    axios({
+        method: 'get',
+        url: `https://mandala-dev.shanti.virginia.edu/oauthtest/usertest`,
         headers: {
-            accept: 'application/json'
+            accept: 'application/json',
+	    Authorization: "Bearer " + access
+	    "X-CSRF-Token": xcsrf
         }
     }).then((response) => {
         // console.log("We got a response!");
         // console.log(response.data);
-        const token_json = response.data;
-
-	console.log( "Probably should do some validation / verification here!");
-	console.dir(token_json);
-        req.session["access_token"] = token_json;
-        res.redirect(`/process`);
+        const data = response.data;
+    	res.send( "We got a response!<p>\n" +
+        "<pre>" + JSON.stringify(data,undefined,2) + "</pre>" );
     }).catch(error => {
         console.log(error);
         next(error);
     });
-})
 
+	
+	res.end();
+    }).catch(error => {
+        console.log(error);
+        next(error);
+    });
+});
+
+// Login path.  Initiates the OAuth ping pong match...
 app.get("/login", (req, res, next) => {
-	res.redirect('https://mandala-dev.shanti.virginia.edu/oauth2/authorize?client_id=test');
+	res.redirect("https://mandala-dev.shanti.virginia.edu/oauth2/authorize?client_id=" + OAuth_clientID);
 });
 
 // Should be authorized now
 app.get("/process", (req, res, next) => {
     // We should have authorization token now
-    const access = req.session["access_token"];  // should be done through session instead of parameter
-    res.send("Hey we got authorization: <pre>" + JSON.stringify(access, undefined, 2) + "</pre>" +
-        "<p>Your Session looks like: " +
-        "<pre>" + JSON.stringify(req.session,undefined,2) + "</pre>" );
 });
 
 // Proxy
 app.use('/solr', proxy('https://ss251856-us-east-1-aws.measuredsearch.com', {  // TODO: configurable base path
-
 
     proxyReqPathResolver: function (req) {
         return new Promise(function (resolve, reject) {
@@ -103,11 +129,11 @@ app.use('/solr', proxy('https://ss251856-us-east-1-aws.measuredsearch.com', {  /
                 queryString += "&"
             }
             queryString += "wt=json";
+            queryString += "wt=json";
             var updatedPath = '/solr' + parts[0];
             var resolvedPathValue = updatedPath + (queryString ? '?' + queryString : '');
             console.log("Resolving with " + resolvedPathValue);
             resolve(resolvedPathValue);
-
 
         });
     }
@@ -116,4 +142,3 @@ app.use('/solr', proxy('https://ss251856-us-east-1-aws.measuredsearch.com', {  /
 
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
